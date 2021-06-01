8ce5d6a1f559236a32849270f554c8a82530e65e
ruthfs <ruh.ruth@gmail.com>
2021-06-01 13:40:06 -0300

    alterações no service

--- blogPessoal/src/app/service/auth.service.ts
+++ blogPessoal/src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
@@ -10,14 +11,14 @@ import { UserLogin } from '../model/UserLogin';
 export class AuthService {
 
   constructor(
-    private http:HttpClient
+    private http: HttpClient
   ) { }
   
   entrar(userLogin:UserLogin): Observable<UserLogin>{
-    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
+    return this.http.post<UserLogin>(`${environment.server}/usuarios/logar`, userLogin)
   }
 
   cadastrar(user: User): Observable<User>{
-    return this.http.post<User>('http://localhost:8080/usuarios/cadastrar',user)
+    return this.http.post<User>(`${environment.server}/usuarios/cadastrar`,user)
   }
 }
--- blogPessoal/src/environments/environment.prod.ts
+++ blogPessoal/src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   token: '',
   nome: '',
   id: 0,
-  foto: ''  
+  foto: '', 
+  server: 'https://ruthblogpessoalgen.herokuapp.com'
 };
