521d1f5f42d1b1c080692ed4d4d5b04d187fb489
ruthfs <ruh.ruth@gmail.com>
2021-06-08 14:25:03 -0300

    implementação do put e delete de postagem

--- blogPessoal/src/app/service/postagem.service.ts
+++ blogPessoal/src/app/service/postagem.service.ts
@@ -19,8 +19,20 @@ export class PostagemService {
     return this.http.get<Postagem[]>(`${environment.server}/postagens`, this.token)
   }
 
+  getByIdPostagem(id: number): Observable<Postagem>{
+    return this.http.get<Postagem>(`${environment.server}/postagens/${id}`, this.token)
+  }
+
   postPostagem(postagem: Postagem): Observable<Postagem>{
     return this.http.post<Postagem>(`${environment.server}/postagens`, postagem, this.token)
     
+  }
+
+  putPostagem(postagem: Postagem): Observable<Postagem>{
+    return this.http.put<Postagem>(`${environment.server}/postagens`, postagem, this.token)
+  }
+
+  deletePostagem(id: number){
+    return this.http.delete(`${environment.server}/postagens/${id}`, this.token)
   }
 }
